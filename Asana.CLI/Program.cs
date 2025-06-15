8aa77382da2dbe8b507e818061763069d1c4cee7
Ingrid Perez <ingrid@Ingrids-MacBook-Air.local>
2025-06-14 21:23:12 -0400

    Fix: add Asana.CLI as part of main repo

---
+++ Asana.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Asana.Library.Models;
+using System;
+
+namespace Asana
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? myName = null;
+
+            Console.WriteLine($"Hello, {myName ?? "Bob" }!");
+            // ?? --> means "if everything on the left had in null do something different. 
+        }
+    }
+}
